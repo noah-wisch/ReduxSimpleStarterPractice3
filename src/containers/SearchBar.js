e38f18924bc5c1f53b5ed910977f61b8c2aa69ce
Noah Wisch <noah.wisch@gmail.com>
2017-05-04 10:57:04 -0400

    searchBar edit

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -14,9 +14,14 @@ export default class SearchBar extends Component {
     this.setState({ term: e.target.value })
   }
 
+  onFormSubmit(e) {
+    e.preventDefault();
+    
+  }
+
   render() {
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forcast in your selected cities"
           className="form-control"
