9b9e16fe8e50d9798dbf0676ddb8dd20dc5029f1
Noah Wisch <noah.wisch@gmail.com>
2017-05-09 10:56:37 -0400

    fixed console error, missing conbineReducer for weather

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import WeatherReducer from './reducerWeather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
